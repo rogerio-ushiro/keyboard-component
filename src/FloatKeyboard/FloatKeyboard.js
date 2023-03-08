2655c958c9794f947ec671f58d8a8b7b48385fea
Rogerio Ushiro <rogerio.ushiro@gmail.com>
2023-03-08 16:02:47 -0300

    solved issue related to useEffect dependency error

--- src/FloatKeyboard/FloatKeyboard.js
+++ src/FloatKeyboard/FloatKeyboard.js
@@ -16,7 +16,7 @@ const FloatKeyboard = forwardRef((props, ref) => {
 
   useEffect(() => {
     getKeyDown(data);
-  }, [data]);
+  }, [data, getKeyDown]);
 
   const send = (evt) => {
     const inputKey = evt.currentTarget.id;
@@ -58,82 +58,193 @@ const FloatKeyboard = forwardRef((props, ref) => {
     }
   };
 
-  return <>
-  
-  <div className={`keyboard ${enable ? "show" : ""}`}  >
-      <ul className="row row-0">
-          <li onClick={()=>{setEnable(false)}} id="esc">
+  return (
+    <>
+      <div className={`keyboard ${enable ? "show" : ""}`}>
+        <ul className="row row-0">
+          <li
+            onClick={() => {
+              setEnable(false);
+            }}
+            id="esc"
+          >
             ESC
           </li>
-          <li onClick={send} id="1">1</li>
-          <li onClick={send} id="2">2</li>
-          <li onClick={send} id="3">3</li>
-          <li onClick={send} id="4">4</li>
-          <li onClick={send} id="5">5</li>
-          <li onClick={send} id="6">6</li>
-          <li onClick={send} id="7">7</li>
-          <li onClick={send} id="8">8</li>
-          <li onClick={send} id="9">9</li>
-          <li onClick={send} id="10">0</li>
-          <li onClick={send} id="-">-</li>
-          <li onClick={send} id="+">+</li>
-          <li onClick={send} id="back">BACK</li>
+          <li onClick={send} id="1">
+            1
+          </li>
+          <li onClick={send} id="2">
+            2
+          </li>
+          <li onClick={send} id="3">
+            3
+          </li>
+          <li onClick={send} id="4">
+            4
+          </li>
+          <li onClick={send} id="5">
+            5
+          </li>
+          <li onClick={send} id="6">
+            6
+          </li>
+          <li onClick={send} id="7">
+            7
+          </li>
+          <li onClick={send} id="8">
+            8
+          </li>
+          <li onClick={send} id="9">
+            9
+          </li>
+          <li onClick={send} id="10">
+            0
+          </li>
+          <li onClick={send} id="-">
+            -
+          </li>
+          <li onClick={send} id="+">
+            +
+          </li>
+          <li onClick={send} id="back">
+            BACK
+          </li>
         </ul>
         <ul className="row row-1">
-          <li onClick={send} id="tab">TAB</li>
-          <li onClick={send} id="Q">Q</li>
-          <li onClick={send} id="W">W</li>
-          <li onClick={send} id="E">E</li>
-          <li onClick={send} id="R">R</li>
-          <li onClick={send} id="T">T</li>
-          <li onClick={send} id="Y">Y</li>
-          <li onClick={send} id="U">U</li>
-          <li onClick={send} id="I">I</li>
-          <li onClick={send} id="O">O</li>
-          <li onClick={send} id="P">P</li>
-          <li onClick={send} id="[">[</li>
-          <li onClick={send} id="]">]</li>
-          <li onClick={send} id="\">\</li>
+          <li onClick={send} id="tab">
+            TAB
+          </li>
+          <li onClick={send} id="Q">
+            Q
+          </li>
+          <li onClick={send} id="W">
+            W
+          </li>
+          <li onClick={send} id="E">
+            E
+          </li>
+          <li onClick={send} id="R">
+            R
+          </li>
+          <li onClick={send} id="T">
+            T
+          </li>
+          <li onClick={send} id="Y">
+            Y
+          </li>
+          <li onClick={send} id="U">
+            U
+          </li>
+          <li onClick={send} id="I">
+            I
+          </li>
+          <li onClick={send} id="O">
+            O
+          </li>
+          <li onClick={send} id="P">
+            P
+          </li>
+          <li onClick={send} id="[">
+            [
+          </li>
+          <li onClick={send} id="]">
+            ]
+          </li>
+          <li onClick={send} id="\">
+            \
+          </li>
         </ul>
         <ul className="row row-2">
-          <li onClick={send} id="caps" className={capsActive ? "active" : ""}>CAPS</li>
-          <li onClick={send} id="A">A</li>
-          <li onClick={send} id="S">S</li>
-          <li onClick={send} id="D">D</li>
-          <li onClick={send} id="F">F</li>
-          <li onClick={send} id="G">G</li>
-          <li onClick={send} id="H">H</li>
-          <li onClick={send} id="J">J</li>
-          <li onClick={send} id="K">K</li>
-          <li onClick={send} id="L">L</li>
+          <li onClick={send} id="caps" className={capsActive ? "active" : ""}>
+            CAPS
+          </li>
+          <li onClick={send} id="A">
+            A
+          </li>
+          <li onClick={send} id="S">
+            S
+          </li>
+          <li onClick={send} id="D">
+            D
+          </li>
+          <li onClick={send} id="F">
+            F
+          </li>
+          <li onClick={send} id="G">
+            G
+          </li>
+          <li onClick={send} id="H">
+            H
+          </li>
+          <li onClick={send} id="J">
+            J
+          </li>
+          <li onClick={send} id="K">
+            K
+          </li>
+          <li onClick={send} id="L">
+            L
+          </li>
           <li>:</li>
           <li>''</li>
-          <li onClick={send} id="enter">ENTER</li>
+          <li onClick={send} id="enter">
+            ENTER
+          </li>
         </ul>
         <ul className="row row-3">
-          <li onClick={send} id="left-shift">SHIFT</li>
-          <li onClick={send} id="Z">Z</li>
-          <li onClick={send} id="X">X</li>
-          <li onClick={send} id="C">C</li>
-          <li onClick={send} id="V">V</li>
-          <li onClick={send} id="B">B</li>
-          <li onClick={send} id="N">N</li>
-          <li onClick={send} id="M">M</li>
-          <li onClick={send} id=",">,</li>
-          <li onClick={send} id=".">.</li>
-          <li onClick={send} id=";">;</li>
-          <li onClick={send} id="right-shift">SHIFT</li>
+          <li onClick={send} id="left-shift">
+            SHIFT
+          </li>
+          <li onClick={send} id="Z">
+            Z
+          </li>
+          <li onClick={send} id="X">
+            X
+          </li>
+          <li onClick={send} id="C">
+            C
+          </li>
+          <li onClick={send} id="V">
+            V
+          </li>
+          <li onClick={send} id="B">
+            B
+          </li>
+          <li onClick={send} id="N">
+            N
+          </li>
+          <li onClick={send} id="M">
+            M
+          </li>
+          <li onClick={send} id=",">
+            ,
+          </li>
+          <li onClick={send} id=".">
+            .
+          </li>
+          <li onClick={send} id=";">
+            ;
+          </li>
+          <li onClick={send} id="right-shift">
+            SHIFT
+          </li>
         </ul>
 
         <ul className="row row-4">
-          <li onClick={send} id="@">@</li>
+          <li onClick={send} id="@">
+            @
+          </li>
           <li onClick={send} id="space"></li>
-          <li onClick={send} id="combr">.COM.BR</li>
-          <li onClick={send} id="com">.COM</li>
+          <li onClick={send} id="combr">
+            .COM.BR
+          </li>
+          <li onClick={send} id="com">
+            .COM
+          </li>
         </ul>
-
       </div>
-      </>;
+    </>
+  );
 });
 
 export default FloatKeyboard;
